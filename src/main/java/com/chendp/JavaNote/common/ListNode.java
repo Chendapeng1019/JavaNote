eaa89947237d851d25eb1466a8e6c074d72dfe69
chendp1 <chendp1@xiaopeng.com>
2023-12-26 02:05:46 +0800

    fix:提交

---
+++ src/main/java/com/chendp/JavaNote/common/ListNode.java
@@ -0,0 +1,14 @@
+package com.chendp.JavaNote.common;
+
+/**
+ * @author chendp
+ * @date 2023/12/24 0:37
+ **/
+public class ListNode {
+    int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
